17187552ce5335903c0a9590cbd079389a858951
tanyaka <mail@tanyaka.net>
2013-09-26 12:35:21 +0200

    custom options added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 $(function(){
-    $( '#magic_zoom img').magicZoom({
+    $( '.magic_zoom').magicZoom({
+			activeThumb: 2,
+			thumpPosition:"HB"
+	});
+	
+	var opts = $('.opt'), opt;
+
+	for (var i = 0; i < opts.length; i++) {
+		opt = opts[i];
+		if( $( opt ).find('em').text() == "HB") $( opt ).addClass('active');
+	}
+
+
+	$('.opt').on( "click", function() {
+		var thumbs =  $('#list').find("img"), thumbPos = $( this ).find('em').text();
+		$( '.magic_zoom').empty();
+    	thumbs.appendTo( '.magic_zoom');
+
+		$( this ).parent().find('.active').removeClass('active');
+		$( this ).addClass('active');
+
+    	$( '.magic_zoom').magicZoom({
 			activeThumb: 1,
-			thumpPosition:"VR"
+			thumpPosition: thumbPos
+		});
+
+
 	});
 });
--- js/plugins.js
+++ js/plugins.js
@@ -1,9 +1,11 @@
 (function ( $ ) {
     var PreviewObj = new Object,
         Settings = new Object,
+        path,
         coord = new Array,
         spacing = 5, widthMax = 0,
-        stop_timeout = false;
+        stop_timeout = false,
+        container, size, interval, zoomedX, zoomedY;
 
     $.fn.magicZoom = function(options) {
         var settings = $.extend({
@@ -11,68 +13,83 @@
             thumpPosition:  "VR"
         }, options );
 
-        var aThumb = this[settings.activeThumb], widthAll = new Array();
+        var aThumb = this[settings.activeThumb], widthAll = new Array(), imgs;
 
-        this.each(function() {widthAll.push(this.width)});
+        container = this;
+
+        imgs = $(this).find("img");
+        aThumb = imgs[settings.activeThumb];
+
+        imgs.each(function() {widthAll.push(this.width); $(this).css('cursor', 'pointer'); });
+        
         widthMax = Math.max.apply(Math, widthAll);
 
-        $(this).remove();
+        $(imgs).remove();
 
         Settings = settings;
 
-        init(this);
-        
-        
-        return this.each(function() {
+        init(imgs);
+    
+        return imgs.each(function() {
+            
+            
             if(this === aThumb) {
-                PreviewObj.fileName = $(this).data( "zoomed" );
-                $(this).addClass('active');
+                PreviewObj.previewName = $(this).data( "preview" );
+                PreviewObj.zoomName = $(this).data( "zoomed" );
+                $(this).css('box-shadow', '0 2px 3px 0 rgba(0, 0, 0, 0.5)');
                 createPreview();
             }
 
             $(this).on("click", function(event){
-                PreviewObj.fileName = $(this).data( "zoomed" );
-                $('.active').removeClass('active');
-                $(this).addClass('active');
+                PreviewObj.previewName = $(this).data( "preview" );
+                PreviewObj.zoomName = $(this).data( "zoomed" );
+                $(imgs).css('box-shadow', '0 1px 2px 0 rgba(0, 0, 0, 0.3)');
+                $(this).css('box-shadow', '0 2px 3px 0 rgba(0, 0, 0, 0.5)');
                 $('#zoom').empty();
                 $('.message').remove();
                 createPreview();
             });
         });
-    };
+    }
 
     function init(imgs) {
 
-        /*$('.message').css({'position': 'absolute', 'margin': '0', 'text-align': 'center', 'vertical-align': 'middle'});
-        $('#zoom').css({'position': 'absolute', 'visibility': 'hidden', 'overflow': 'hidden'});
-        $('.preview').css({'position': 'absolute'});
-        $('.zoom').css({'position': 'absolute'});*/
-
+        path = ($(imgs[0]).attr('src')).split('/').reverse().slice(1).reverse().join('/');
 
         if (Settings.thumpPosition == "VR" || Settings.thumpPosition == "VRB") {
-            $('<div id=list /><div id=preview /><div id=zoom />').appendTo($('#magic_zoom'));
+            $('<div id=list /><div id=preview /><div id=zoom />').appendTo($(container));
             $('#list').css({'width':widthMax+2*spacing, 'float':'left'});
             $('#preview').css('float','left');
         } else if (Settings.thumpPosition == "HB") {
-            $('<div id=preview /><div id=list /><div id=zoom />').appendTo($('#magic_zoom'));
+            $('<div id=preview /><div id=list /><div id=zoom />').appendTo($(container));
         } else if (Settings.thumpPosition == "HT" || Settings.thumpPosition == "HTB") {
-            $('<div id=list /><div id=preview /><div id=zoom />').appendTo($('#magic_zoom'));
+            $('<div id=list /><div id=preview /><div id=zoom />').appendTo($(container));
         }
 
+        $('#preview').css('box-shadow', '0 1px 2px 0 rgba(0, 0, 0, 0.3)');
+        
+        $('#zoom').css({'position': 'absolute', 'visibility': 'hidden', 'overflow': 'hidden', 'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.3)'});
+
+        
         $(imgs).appendTo($('#list'));
 
+
+        
+//#list img.active { box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.5);}
+        
         if(Settings.thumpPosition == "VR" || Settings.thumpPosition == "VRB") 
-            $(imgs).css({'display':'block', 'margin': '0 '+2*spacing+'px '+spacing+'px 0'});
+            $(imgs).css({'display':'block', 'margin': '0 '+2*spacing+'px '+spacing+'px 0', 'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.3)'});
         else if (Settings.thumpPosition == "HB") 
-            $(imgs).css({'margin': spacing+'px '+spacing+'px  '+2*spacing+'px 0'});
+            $(imgs).css({'display':'inline','margin': spacing+'px '+spacing+'px  '+2*spacing+'px 0', 'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.3)'});
         else if (Settings.thumpPosition == "HT" || Settings.thumpPosition == "HTB")
-            $(imgs).css({'margin': '0 '+spacing+'px '+spacing+'px 0'});
+            $(imgs).css({'display':'inline','margin': '0 '+spacing+'px '+spacing+'px 0', 'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.3)'});
+
     }
 
     function createPreview() {
-        var url = 'img/vincentfoutnier/'+PreviewObj.fileName+'.jpg',
+        var url = path+'/'+PreviewObj.previewName,
             previewImg = $('<img class=preview src="'+url+'">');
-            previewImg.css({'visibility':'hidden'});
+            previewImg.css({'position': 'absolute','visibility':'hidden'});
         previewImg.appendTo($('#preview'));
 
         $(previewImg).one('load', function() {
@@ -110,9 +127,10 @@
 
     function initZoom() {
         
-        var url = 'img/vincentfoutnier/zoom_'+PreviewObj.fileName+'.jpg',
+        var url = path+'/'+PreviewObj.zoomName,
             zoomImg = $('<img class ="zoom" src="'+url+'">');
         zoomImg.appendTo($('#zoom'));
+        zoomImg.css('position', 'absolute');
 
         displayMessage('...load zoom...');
         
@@ -133,10 +151,13 @@
     function displayMessage(msg, clr) {
         clr = typeof clr !== 'undefined' ? clr : '#000';
         var target= $('#preview');
-        if($('.message').length>0) 
+
+        if($('.message').length>0) {
             $('.message').text('...failed to load zoom...'); 
-        else 
+        } else {
             $('<div class=message>'+msg+'</div>').appendTo(target);
+            $('.message').css({'position': 'absolute', 'margin': '0', 'text-align': 'center', 'vertical-align': 'middle'});
+        }
         
         $('.message').css({'width': PreviewObj.previewImgW, 'line-height':PreviewObj.previewImgH+'px', color:clr});
 
@@ -145,12 +166,16 @@
     function calcZoom() {
         $(".preview").mousemove(function(event) {
             var x = event.pageX-$(this).offset().left,
-                y = event.pageY-$(this).offset().top,
+                y = event.pageY-$(this).offset().top, 
+                dx, dy;
                 zoomedX = -Math.round(x*(PreviewObj.zoomImgW-PreviewObj.previewImgW)/PreviewObj.previewImgW),
                 zoomedY = -Math.round(y*(PreviewObj.zoomImgH-PreviewObj.previewImgH)/PreviewObj.previewImgH);
-                TweenLite.to($('.zoom'), 0.2, { css: { left: zoomedX, top: zoomedY }});
+            $('.zoom').css({ left: zoomedX, top: zoomedY });
+            //$('.zoom').animate({ left: zoomedX, top: zoomedY }, 1);
+            //TweenLite.to($('.zoom'), 0.2, { css: { left: zoomedX, top: zoomedY }});
+
         });
-        
+
         $(".preview").mouseover(function(event) {
             $('#zoom').css('visibility','visible');
         });
@@ -158,6 +183,16 @@
         $(".preview").mouseout(function(event) {
             $('#zoom').css('visibility','hidden');
         });
+
+    function moveTo(dx, dy) {
+
+        
+        $('.zoom').css({'left':$('.zoom').offset().left+dx, 'top':$('.zoom').offset().top+dy});
+
+        if( Math.abs($('.zoom').offset().left - zoomedX) < 1 && Math.abs( $('.zoom').offset().top - zoomedY) < 1) clearInterval(interval);
+    }
+
+
     }
 }( jQuery ));
 
